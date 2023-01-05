801107e41fd1c14a4badb28ae28e33b3ec26ad1b
Julie Knox <julie.knox.cohort.225@gmail.com>
2023-01-04 19:04:31 -0800

    finished classes and added examples

---
+++ app.js
@@ -0,0 +1,56 @@
+class Character {
+    constructor(_name, _health, _atkDamage, _npc = true) {
+        this.name = _name;
+        this.health = _health;
+        this.atkDamage = _atkDamage;
+        this.npc = _npc;
+    }
+
+    get _name() {
+        return this.name;
+    }
+
+    get _health() {
+        return this.health;
+    }
+
+    get _atkDamage() {
+        return this.atkDamage;
+    }
+}
+
+class Player extends Character {
+    constructor(_name, _health, _atkDamage, _startingItem) {
+        super(_name, _health, _atkDamage, false);
+
+        this.inventory = [_startingItem];
+    }
+
+    get inventory(){
+        return this.inventory;
+    }
+
+    set addItem(item) {
+        this.inventory.push(item);
+    }
+}
+
+class Enemy extends Character {
+    constructor(_name, _health, _atkDamage, _lvl, _loot) {
+        super(_name, _health, _atkDamage, false);
+
+        this.lvl = _lvl;
+        this.loot = _loot;
+    }
+}
+
+//Example of uses
+const villager = new Character('villager', 3, 0);
+
+const hero = new Player('Julie', 100, 100, 'healing potion');
+
+const spider = new Enemy('spider', 50, 100, 1, 'cobwebs');
+
+console.log(villager);
+console.log(hero);
+console.log(spider);
