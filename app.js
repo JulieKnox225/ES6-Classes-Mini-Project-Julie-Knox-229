9c51fb599b00178c1f722131d89323735f39178e
Julie Knox <julie.knox.cohort.225@gmail.com>
2023-01-04 19:30:13 -0800

    fixed getters and setters and added attacking

--- app.js
+++ app.js
@@ -14,8 +14,32 @@ class Character {
         return this.health;
     }
 
+    set _health(damage) {
+        this.health - damage;
+    }
+
     get _atkDamage() {
         return this.atkDamage;
+    }
+
+    attack(char) {
+        while(this.health > 0 && char._health() > 0) {
+            char._health(this.atkDamage);
+
+            if(char._health() > 0) {
+                this._health(char._atkDamage());
+            }
+        }
+        let winner;
+        let loser;
+        if(char._health() > this.health) {
+            winner = char._name();
+            loser = this.name;
+        } else {
+            winner = this.name;
+            loser = char._name();
+        }
+        console.log(winner + ' has slain ' + loser);
     }
 }
 
@@ -26,11 +50,19 @@ class Player extends Character {
         this.inventory = [_startingItem];
     }
 
+    get items(){
+        return this.inventory;
+    }
+
+    set items(item) {
+        this.inventory.push(item);
+    }
+
 }
 
 class Enemy extends Character {
     constructor(_name, _health, _atkDamage, _lvl, _loot) {
-        super(_name, _health, _atkDamage, false);
+        super(_name, _health, _atkDamage);
 
         this.lvl = _lvl;
         this.loot = _loot;
@@ -48,3 +80,5 @@ const spider = new Enemy('spider', 50, 100, 1, 'cobwebs');
 console.log(villager);
 console.log(hero);
 console.log(spider);
+
+hero.attack(spider);
